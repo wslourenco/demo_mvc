5ba0bfe39fa3bd31365728349590a60d5deedc63
Wilson Lourenço <49756885+wslourenco@users.noreply.github.com>
2026-04-24 21:30:03 -0300

    Atualiza Spring Boot e implementa metodo em DepartamentoDAO

--- src/main/java/com/curso/boot/dao/DepartamentoDAOImpl.java
+++ src/main/java/com/curso/boot/dao/DepartamentoDAOImpl.java
@@ -6,4 +6,14 @@ import com.curso.boot.domain.Departamento;
 @Repository
 public class DepartamentoDAOImpl extends AbstractDAO<Departamento, Long> implements DepartamentoDAO {
 
+    @Override
+    public boolean departamentoTemCargo(Long id) {
+        Long quantidade = getEntityManager()
+                .createQuery("select count(c) from Cargo c where c.departamento.id = :id", Long.class)
+                .setParameter("id", id)
+                .getSingleResult();
+
+        return quantidade != null && quantidade > 0;
+    }
+
 }
