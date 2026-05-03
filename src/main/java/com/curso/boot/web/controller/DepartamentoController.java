5a6eb0b88d4a66adf99c3e627b775fcb778a7193
Wilson Lourenço <49756885+wslourenco@users.noreply.github.com>
2026-05-03 12:47:30 -0300

    Inicial

--- src/main/java/com/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/curso/boot/web/controller/DepartamentoController.java
@@ -1,12 +1,21 @@
 package com.curso.boot.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.curso.boot.domain.Departamento;
+import com.curso.boot.service.DepartamentoService;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
+
+    @Autowired
+    private DepartamentoService service;
 
     @GetMapping("/cadastrar")
     public String cadastrar() {
@@ -16,5 +25,11 @@ public class DepartamentoController {
     @GetMapping("/listar")
     public String listar() {
         return "departamento/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento) {
+        service.salvar(departamento);
+        return "redirect:/departamentos/cadas";
     }
 }
